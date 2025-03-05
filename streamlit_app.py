ea89faea2e976d3a425e2864f1bfc3a4e3a26a0c
rakshub <108409142+rakshub@users.noreply.github.com>
2025-03-05 10:59:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ target_mapper = {'Adelie': 0,
 def target_encode(val):
   return target_mapper[val]
 
-y = y_raw.apply(target_encode)
+y = y.apply(target_encode)
 
 with st.expander('Data preparation'):
   st.write('**Encoded X (input penguin)**')
